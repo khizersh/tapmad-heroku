d7aad36242282081ad019ea687ae3db4bdb9e872
khizer <moh.khizer61@gmail.com>
2020-12-18 05:15:03 -0800

    try1

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,15 @@ import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 import HomePage from "../components/HomePage";
 export default function Home(props) {
-  // console.log("home page props:" , props);
-  // const [data, setData] = useState([]);
-  // fetch("https://jsonplaceholder.typicode.com/todos")
-  //   .then((response) => response.json())
-  //   .then((json) => setData(json));
 
   return (
     <div>
-    <Head>
-      <title>Tapmad</title>
- <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <HomePage {...props} />
-  </div>
+      <Head>
+        <title>Tapmad</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomePage {...props} />
+    </div>
   );
 }
 export async function getStaticProps() {
@@ -26,7 +21,7 @@ export async function getStaticProps() {
   var movie = await movieList.json();
   return {
     props: {
-      movies: movie.Tabs[0].Sections
-    }
+      movies: movie.Tabs[0].Sections,
+    },
   };
 }
