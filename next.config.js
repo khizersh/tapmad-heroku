65e3663aebd0df49bd2c059a6afee85c95e24c92
Ali Wahab <alimemonsd@gmail.com>
2020-12-22 17:09:01 +0500

    Hot fix

--- next.config.js
+++ next.config.js
@@ -8,3 +8,28 @@ module.exports = withPWA({
     sw: "/sw.js"
   }
 });
+
+const withOffline = require("next-offline");
+
+module.exports = withOffline({
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /.png$/,
+        handler: "CacheFirst"
+      },
+      {
+        urlPattern: /api/,
+        handler: "NetworkFirst",
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+            headers: {
+              "x-test": "true"
+            }
+          }
+        }
+      }
+    ]
+  }
+});
