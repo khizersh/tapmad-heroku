01ddf1ea77843c36b96e9ed8d968592fc7fbc79e
Ali Wahab <alimemonsd@gmail.com>
2020-12-22 21:22:44 +0500

    Hot fix

--- next.config.js
+++ next.config.js
@@ -1,35 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    disable: false,
-    register: true,
-    scope: "/",
-    sw: "/sw.js"
-  }
-});
-
-const withOffline = require("next-offline");
-
-module.exports = withOffline({
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: /.png$/,
-        handler: "CacheFirst"
-      },
-      {
-        urlPattern: /api/,
-        handler: "NetworkFirst",
-        options: {
-          cacheableResponse: {
-            statuses: [0, 200],
-            headers: {
-              "x-test": "true"
-            }
-          }
-        }
-      }
-    ]
+    disable: isProd,
+    dest: "public"
   }
 });
