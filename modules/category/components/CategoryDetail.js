db8e61726992b27eba98da1a2c855f044ab4e304
Ali Wahab <alimemonsd@gmail.com>
2021-01-20 14:25:18 +0500

    Fixed syno page background issue

--- modules/category/components/CategoryDetail.js
+++ modules/category/components/CategoryDetail.js
@@ -2,32 +2,37 @@ import React from "react";
 import Card from "./card/Card";
 
 const CategoryDetail = ({ video, videoList }) => {
+  console.log(video);
   return (
     <>
       <div className="row">
-        <div className="col-sm-12">
+        <div className="col-sm-12 mt-5">
           <div className="row">
-            <div className="col-lg-6 col-md-6 col-12 col-sm-12">
-              <h2>{video && video.VideoName}</h2>
-              <div className="text-white">
-                {video && video.VideoDescription}
-              </div>
-              <div>
-                <button className="btn btn-primary">Play</button>
-              </div>
-            </div>
-            <div className="col-lg-6 col-md-6 col-12 col-sm-12 d-none d-sm-none d-lg-block d-md-block">
+            <div className="col-lg-12 col-md-12 col-12 col-sm-12">
               {video && video["NewVideoImageThumbnail"] ? (
-                <div>
-                  <img src={video["NewVideoImageThumbnail"]} />
-                </div>
+                <div
+                  style={{
+                    background: `url(${video["VideoImagePathLarge"]})`,
+                    height: "400px",
+                    border: "1px solid black",
+                  }}
+                ></div>
               ) : null}
-            </div>
-            <div className="col-12">
               <div
                 className="bg-color"
                 style={{ width: "100%", height: "600px" }}
               ></div>
+              <div className="video-syno-text">
+                <h2>{video && video.VideoName}</h2>
+                <div>{video && video.VideoCategoryName}</div>
+                <div className="text-white">
+                  {video && video.VideoDescription}
+                </div>
+                <br />
+                <div>
+                  <button className="btn btn-primary">Play</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
