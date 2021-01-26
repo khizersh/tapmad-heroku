e0b4188e4265b313d72438c428c3c3e7fca59b2f
Ali Memon <alimemonsd@gmail.com>
2021-01-26 14:04:14 +0500

    after sign up page layout fix

---
+++ modules/auth/components/DropdownWithImage.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const DropdownWithImage = ({ data, onChange }) => {
+  const [selected, setSelected] = useState(null);
+  const [isCaretOpen, setIsCaretOpen] = useState(false);
+
+  const onSelectItem = (data) => {
+    onChange(data);
+    setSelected(data);
+    setIsCaretOpen(false);
+  };
+  return (
+    <div>
+      <div className=" form-control width-11rem">
+        <div onClick={() => setIsCaretOpen(!isCaretOpen)} className="textfirst">
+          {selected ? (
+            <div className="">
+              <img src={selected.src} width="20" />{" "}
+              <span className="pl-1">{selected.label}</span>
+              <img
+                src={
+                  isCaretOpen
+                    ? "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-up-b-128.png"
+                    : "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-128.png"
+                }
+                width="10"
+                height="10"
+                className="caret-down"
+              />
+            </div>
+          ) : (
+            <>
+              {" "}
+              Select
+              <img
+                src={
+                  isCaretOpen
+                    ? "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-up-b-128.png"
+                    : "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-128.png"
+                }
+                width="10"
+                height="10"
+                className="caret-down"
+              />
+            </>
+          )}
+        </div>
+      </div>
+      <div
+        className={`overflow-hidden position-absolute dropdown-width ${
+          isCaretOpen ? "translate-100" : "translate-0"
+        } `}
+      >
+        <div className={`clr-black`}>
+          {data.length
+            ? data.map((d, i) => (
+                <div
+                  key={i}
+                  onClick={() => onSelectItem(d)}
+                  className="cursor-pointer border-bottom padding-left-bottom"
+                >
+                  <img src={d.src} width="20" />{" "}
+                  <span className="pl-1">{d.label}</span>
+                </div>
+              ))
+            : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DropdownWithImage;
---
+++ modules/auth/components/SignMessage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const SignMessage = ({ type }) => {
+  if (type == "simCard") {
+    return (
+      <div className="form-group text-center pt-2 mb-0">
+        <button className="btn pymnt_pge_sbscrbe_btn">Subscribe</button>
+        <p style={{ color: "#fff", padding: "10px" }}>
+          Subscribe button press karne pe ek din Free trial hasil karain. Apki
+          subscription charges ek din ke baad lago honge.
+        </p>
+      </div>
+    );
+  } else if (type == "easyPaisa") {
+    return (
+      <div className="form-group text-center pt-2 mb-0">
+        <button className="btn pymnt_pge_sbscrbe_btn">Subscribe</button>
+        <p style={{ color: "#fff", padding: "10px" }}>
+          Pay now button press karnay par Rs.5+ tax aap k easypaisa account say
+          kat jaengyYe subscription 1 month ke leye hai jis ke baad
+          Rs.100+tax/month charge ho ga
+        </p>
+      </div>
+    );
+  } else if (type == "jazzCash") {
+    return (
+      <div className="form-group text-center pt-2 mb-0">
+        <button className="btn pymnt_pge_sbscrbe_btn">Subscribe</button>
+        <p style={{ color: "#fff", padding: "10px" }}>
+          Pay now button press karnay par Rs.100+ tax aap k jazzcash account say
+          kat jaengy Ye subscription 1 month ke leye hai
+        </p>
+      </div>
+    );
+  }
+};
+
+export default SignMessage;
