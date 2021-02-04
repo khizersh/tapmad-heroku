4cfe77c15a7477d60cecc481a562967b7ffb31a3
Ali Wahab <alimemonsd@gmail.com>
2021-02-04 15:50:09 +0500

    Added pin field

--- modules/auth/sign-up/Pin.js
+++ modules/auth/sign-up/Pin.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Pin = () => {
-    return (
-        <div>
+  return (
+    <div className="text-center">
+      <div className="py-3">
+        <label class="text-muted">Please verify your OTP Code</label>
+      </div>
+      <div className="px-3 pb-4">
         <input type="text" placeholder="Enter Pin" className="form-control" />
-        </div>
-    )
-}
+      </div>
+      <div className="pb-4">
+        <button type="button" className="btn btn-primary ">
+          Verify OTP
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Pin
+export default Pin;
