f24e137c8a0d66b04a564ea9c54b351220bf2b83
Ali Memon <alimemonsd@gmail.com>
2021-02-17 18:14:54 +0500

    sign up module

--- pages/movies.js
+++ pages/movies.js
@@ -1,31 +1,32 @@
-import Head from "next/head";
-import Movies from "../modules/movies/components/movies";
-import { get } from "../services/http-service";
-export default function MoviesPage(props) {
-  return (
-    <div>
-      <Head>
-        <title>
-          Tapmad - Watch LIVE TV Channels Online | Watch Pakistani tv Channels
-          Free
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <div>
-        <Movies {...props} />
-      </div>
-    </div>
-  );
-}
-export async function getStaticProps() {
-  var moviesList = await get(
-    "https://api.tapmad.com/api/getMoviesWithPagination/0/5/0/16"
-  );
-  var movies = await moviesList.data;
-  return {
-    props: {
-      movies: movies,
-    },
-  };
-}
+// import Head from "next/head";
+// import Movies from "../modules/movies/components/movies";
+// import { get } from "../services/http-service";
+// export default function MoviesPage(props) {
+//   return (
+//     <div>
+//       <Head>
+//         <title>
+//           Tapmad - Watch LIVE TV Channels Online | Watch Pakistani tv Channels
+//           Free
+//         </title>
+//         <link rel="icon" href="/favicon.ico" />
+//         <link rel="manifest" href="/manifest.json" />
+//       </Head>
+//       <div>
+//         <Movies {...props} />
+//       </div>
+//     </div>
+//   );
+// }
+// export async function getStaticProps() {
+//   var moviesList = await get(
+//     "https://api.tapmad.com/api/getMoviesWithPagination/0/5/0/16"
+//   );
+//   var movies = await moviesList.data;
+//   console.log("moviesList: ", moviesList);
+//   return {
+//     props: {
+//       movies: movies,
+//     },
+//   };
+// }
