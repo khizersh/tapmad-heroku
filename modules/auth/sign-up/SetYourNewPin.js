fa74875f009e234d179e11872661be9eba3dfffe
khizer <khizeroff61@gmail.com>
2021-04-26 12:14:16 +0500

    after set new pin fix

--- modules/auth/sign-up/SetYourNewPin.js
+++ modules/auth/sign-up/SetYourNewPin.js
@@ -47,7 +47,7 @@ export default function SetYourNewPin() {
       Language: "en",
     });
     console.log("statusstatusstatus: ", status);
-    if (status.data.User) {
+    if (status && status.data.User) {
       obj.UserPassword = status.data.User.UserPassword;
       Cookie.setCookies("userId", status.data.User.UserId);
       Cookie.setCookies("content-token", status.data.User.UserPassword);
