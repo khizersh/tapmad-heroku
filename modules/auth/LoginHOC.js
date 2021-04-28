1e9fa0b5597260fc1234f4598ddd83cea098635f
khizer <khizeroff61@gmail.com>
2021-04-28 13:26:26 +0500

    test 1

--- modules/auth/LoginHOC.js
+++ modules/auth/LoginHOC.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { LoginTag } from "../../services/gtm";
 import { AuthService } from "./auth.service";
 import { useRouter } from "next/router";
@@ -41,6 +40,7 @@ export default function withLogin(Component, data) {
         Cookie.setCookies("userId", response.data.UserId);
         Cookie.setCookies("user_mob", encryptWithAES(obj.MobileNo));
         LoginTag(obj, response.response);
+        console.log("safaf");
         checkUserAuthentication();
         let backURL = Cookie.getCookies("backUrl") || "/";
         if (backURL == "sign-in") {
@@ -63,68 +63,3 @@ export default function withLogin(Component, data) {
     return <Component login={loginUser} {...props} />;
   };
 }
-=======
-import { LoginTag } from "../../services/gtm";
-import { AuthService } from "./auth.service";
-import { useRouter } from "next/router";
-import swal from "sweetalert";
-import { Cookie } from "../../services/cookies";
-import { useContext } from "react";
-import { MainContext } from "../../contexts/MainContext";
-import { encryptWithAES } from "../../services/utils";
-
-export default function withLogin(Component, data) {
-  return (props) => {
-    const { checkUserAuthentication, setLoader, initialState } = useContext(
-      MainContext
-    );
-    const router = useRouter();
-
-    async function loginUser() {
-      let obj = {
-        Language: "en",
-        Platform: "web",
-        Version: "V1",
-        MobileNo: initialState.User.MobileNo,
-        OperatorId: initialState.User.OperatorId,
-        UserPassword: initialState.User.Password,
-      };
-      let response = await AuthService.signInOrSignUpMobileOperator(
-        obj,
-        "",
-        false
-      );
-      if (response && response.data && response.data.UserId) {
-        swal({
-          timer: 2000,
-          title: "Signed In Successfully",
-          text: "Redirecting you...",
-          icon: "success",
-        });
-        Cookie.setCookies("isAuth", 1);
-        Cookie.setCookies("userId", response.data.UserId);
-        Cookie.setCookies("user_mob", encryptWithAES(obj.MobileNo));
-        LoginTag(obj, response.response);
-        checkUserAuthentication();
-        let backURL = Cookie.getCookies("backUrl") || "/";
-        if (backURL == "sign-in") {
-          router.push("/");
-        } else {
-          router.push(backURL);
-        }
-        setLoader(false);
-        return null;
-      } else {
-        setLoader(false);
-        swal({
-          title: response.message,
-          icon: "error",
-          timer: 3000,
-        });
-        return response;
-      }
-    }
-    return <Component login={loginUser} {...props} />;
-  };
-}
->>>>>>> 123ba49e587147c6afc161c3dfb216121d3aa47d
