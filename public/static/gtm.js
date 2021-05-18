45dfc2e9cc12286ad8240954fe1c302fef00d401
Ali Memon <alimemonsd@gmail.com>
2021-05-18 16:42:37 +0500

    Changed GTM

--- public/static/gtm.js
+++ public/static/gtm.js
@@ -5,4 +5,4 @@
     }); var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
             'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-W3VZGWZ');
+})(window, document, 'script', 'dataLayer', 'GTM-PJ4M57N');
