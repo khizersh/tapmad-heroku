9e1813570382b0f74eefa235cd7c1ee869465675
Ali Memon <alimemonsd@gmail.com>
2021-06-02 15:15:19 +0500

    Fixed UI of game

--- components/psl/psl-component.js
+++ components/psl/psl-component.js
@@ -38,16 +38,23 @@ export default memo(function PSLComponent({ channelID }) {
             return <PSLChat channelID={channelID} />
         } else if (selectedTab == 2) {
             // return <MatchBids />;
-            return <iframe src="http://localhost:3000/game" id="gameFrame" style={{ width: "100%", height: "500px", border: "0px" }} />;
+            return <iframe allow src="/game" id="gameFrame" style={{ width: "100%", height: "500px", border: "0px" }} />;
         } else {
             return <div></div>;
         }
     })
     useEffect(() => {
         var frameObj = document.getElementById('gameFrame');
+        var contents = "";
+
         if (frameObj) {
-            frameObj = frameObj.contentDocument || frameObj.contentWindow.document;;
-            console.log(frameObj)
+            frameObj.onload = (() => {
+                contents = frameObj.contentDocument || frameObj.contentWindow.document;
+                console.log(contents)
+                contents.getElementsByClassName('fixed-bottom')[0].style.display = "none";
+                contents.getElementsByClassName('scrolling-navbar')[0].style.display = "none";
+                contents.getElementsByClassName('primary-nav')[0].style.display = "none";
+            })
         }
     }, [selectedTab])
     return <>
