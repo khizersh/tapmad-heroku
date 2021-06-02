ff0b548752117967efbbbca32bdb5a2bab96f2d1
khizer <khizer.younus@tapmad.com>
2021-06-02 16:01:49 +0500

    merge

--- modules/game/components/my-bids.js
+++ modules/game/components/my-bids.js
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { getMatchBetsByUser } from "../../../components/psl/bids/bids.service";
 import styles from "../game.module.css";
 import CustomCollapse from "./CustomCollapse";
+import { Accordion, Card } from "react-bootstrap";
 
-const MyBid = () => {
+const MyBid = ({ type }) => {
   const [data, setData] = useState([]);
-
   useEffect(() => {
     getMatchBetsByUser()
       .then((res) => {
         if (res && res.Response.responseCode == 1) {
           setData([
             {
-              id: "collapse1",
+              id: "play",
               title: "In Play",
               icon: "//d1s7wg2ne64q87.cloudfront.net/web/images/coin-white.png",
               content: res.InPlay || "No In Play",
             },
             {
-              id: "collapse2",
+              id: "won",
               title: "Won",
               icon: "//d1s7wg2ne64q87.cloudfront.net/web/images/trophy-white.png",
               content: res.Won || "No Win",
             },
             {
-              id: "collapse3",
+              id: "lost",
               title: "Lost",
               icon: "//d1s7wg2ne64q87.cloudfront.net/web/images/lost-white.png",
               content: res.Lost || "No Lost",
             },
             {
-              id: "collapse4",
+              id: "draw",
               title: "No Result/Draw",
               icon: "//d1s7wg2ne64q87.cloudfront.net/web/images/coin-draw-white.png",
               content: res.NoResult || "No draw matches data",
@@ -39,7 +39,7 @@ const MyBid = () => {
         }
       })
       .catch((e) => console.log(e));
-  }, []);
+  }, [type]);
 
   return (
     <div>
@@ -49,14 +49,23 @@ const MyBid = () => {
           id="accordionEx"
           role="tablist"
           aria-multiselectable="true"
+          className={styles.width}
         >
-          {data.length
-            ? data.map((m, i) => (
-                <div className={styles.width}>
-                  <CustomCollapse data={m} />
-                </div>
-              ))
-            : null}
+          {type ? (
+            <Accordion className="mt-3" activeKey={type}>
+              {
+              
+              data.length
+                ? data.map((m, i) => <CustomCollapse data={m} />)
+                : null}
+            </Accordion>
+          ) : (
+            <Accordion className="mt-3">
+              {data.length
+                ? data.map((m, i) => <CustomCollapse data={m} />)
+                : null}
+            </Accordion>
+          )}
         </div>
       </div>
     </div>
