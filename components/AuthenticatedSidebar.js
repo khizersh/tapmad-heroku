50ed7b96dfebe035d09e35bd9138fce4a3738e2c
khizer <khizer.younus@tapmad.com>
2021-06-08 21:28:39 +0500

    test 1

--- components/AuthenticatedSidebar.js
+++ components/AuthenticatedSidebar.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { MainContext } from "../contexts/MainContext";
 import withSignout from "../modules/auth/signout/SignoutHOC";
 import { loggingTags } from "../services/apilinks";
 import { actionsRequestContent } from "../services/http-service";
 
 const AuthenticatedSidebarBasic = ({ signout }) => {
+  const { initialState } = useContext(MainContext);
+  const [game, setGame] = useState(false);
+
   const onCLickContent = (page) => {
     let body = {
       event: loggingTags.fetch,
@@ -12,6 +16,19 @@ const AuthenticatedSidebarBasic = ({ signout }) => {
     };
     actionsRequestContent(body);
   };
+
+  useEffect(() => {
+    // if (initialState.countryCode && initialState.countryCode == "PK") {
+    console.log("initialState: ", initialState);
+    if (
+      initialState &&
+      initialState.AuthDetails &&
+      initialState.AuthDetails.CountryCode == "PK"
+    ) {
+      setGame(true);
+    }
+  }, [initialState.AuthDetails]);
+
   return (
     <>
       <li className="logouts_contain" onClick={() => onCLickContent("profile")}>
@@ -24,20 +41,25 @@ const AuthenticatedSidebarBasic = ({ signout }) => {
           </a>
         </Link>
       </li>
-      <li
-        className="sideBarGame"
-        style={{ display: "list-item" }}
-        onClick={() => onCLickContent("game")}
-      >
-        <Link href="/game" shallow={true} passHref={true}>
-          <a>
-            Game
-            <span className="icon">
-              <i className="fa fa-gamepad"></i>
-            </span>
-          </a>
-        </Link>
-      </li>
+      {game ? (
+        <li
+          className="sideBarGame"
+          style={{ display: "list-item" }}
+          onClick={() => onCLickContent("game")}
+        >
+          <Link href="/game" shallow={true} passHref={true}>
+            <a>
+              Game
+              <span className="icon">
+                <i className="fa fa-gamepad"></i>
+              </span>
+            </a>
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+
       <li className="sign-out">
         <a onClick={signout}>
           Signout
