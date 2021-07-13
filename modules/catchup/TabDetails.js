4b009f0988dc42667e0042a3fe23f5dcb1851136
Ali Memon <alimemonsd@gmail.com>
2021-07-13 14:34:21 +0500

    Hot Event fix

--- modules/catchup/TabDetails.js
+++ modules/catchup/TabDetails.js
@@ -6,6 +6,7 @@ import {
   basicSliderConfig,
   SEOFriendlySlugsForCatchupVideo,
 } from "../../services/utils";
+import { UserEngagemnent } from "../../services/gtm";
 
 const TabDetails = () => {
   const [details, setDetails] = useState(null);
@@ -34,45 +35,45 @@ const TabDetails = () => {
     <div>
       {details && details.sections.length
         ? details.sections.map((m, i) => (
-            <div key={i} className="row my-1">
-              <div className=" col-md-2 col-sm-3 pr-0">
-                <div className="dys">
-                  <h4>{m.SectionName.split(" ")[0]}</h4>
-                  <p className="text-muted">
-                    {m.SectionName.split(" ")[1]}
-                  </p>{" "}
-                </div>
-              </div>
-              <div></div>
-
-              <div className="col-md-10 col-sm-9">
-                <Slider {...settings}>
-                  {m.Videos &&
-                    m.Videos.length &&
-                    m.Videos.map((n, i) => {
-                      let slug = SEOFriendlySlugsForCatchupVideo(n);
-                      return (
-                        <Link key={i} href={slug} shallow passHref>
-                          <a onClick={() => onClick(n)}>
-                            <div className="pos-rel">
-                              <img
-                                className="cont-image"
-                                src={n.VideoImageThumbnail}
-                                width="100%"
-                                alt={n.VideoName}
-                              />
-                              {n.IsVideoFree ? null : (
-                                <div className="live_side">{n.PackageName}</div>
-                              )}
-                            </div>
-                          </a>
-                        </Link>
-                      );
-                    })}
-                </Slider>
+          <div key={i} className="row my-1">
+            <div className=" col-md-2 col-sm-3 pr-0">
+              <div className="dys">
+                <h4>{m.SectionName.split(" ")[0]}</h4>
+                <p className="text-muted">
+                  {m.SectionName.split(" ")[1]}
+                </p>{" "}
               </div>
             </div>
-          ))
+            <div></div>
+
+            <div className="col-md-10 col-sm-9">
+              <Slider {...settings}>
+                {m.Videos &&
+                  m.Videos.length &&
+                  m.Videos.map((n, i) => {
+                    let slug = SEOFriendlySlugsForCatchupVideo(n);
+                    return (
+                      <Link key={i} href={slug} shallow passHref>
+                        <a onClick={() => (onClick(n), UserEngagemnent("Catchup", m.SectionName.split(" ")[0], i + 1, n.VideoName))}>
+                          <div className="pos-rel">
+                            <img
+                              className="cont-image"
+                              src={n.VideoImageThumbnail}
+                              width="100%"
+                              alt={n.VideoName}
+                            />
+                            {n.IsVideoFree ? null : (
+                              <div className="live_side">{n.PackageName}</div>
+                            )}
+                          </div>
+                        </a>
+                      </Link>
+                    );
+                  })}
+              </Slider>
+            </div>
+          </div>
+        ))
         : null}
     </div>
   );
