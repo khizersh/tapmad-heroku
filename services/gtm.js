bc5f29215d4f96619e314540c4371cddaff98309
Ali Memon <alimemonsd@gmail.com>
2021-07-13 12:56:52 +0500

    Added new genres

--- services/gtm.js
+++ services/gtm.js
@@ -56,8 +56,12 @@ export function ContentViewed(video) {
 export function VideoWatched(response) {
     if (response.Video && response.Video.getProductiongenreName) {
         var Genre = [];
+        var newGenre = [];
         response.Video.getProductiongenreName.forEach((e) => {
             Genre.push(e.genraName);
+        });
+        response.Video.getProductionNewgenreName.forEach((e) => {
+            newGenre.push(e.newGenraName);
         });
         const { mobile, userId } = getUserDetails();
 
@@ -66,10 +70,13 @@ export function VideoWatched(response) {
             ID: response.Video.VideoEntityId,
             Name: response.Video.VideoName,
             Genre: Genre.toString(),
+            NewGenre: newGenre.toString(),
             Category: response.Video.productioncategoryName,
             Productionhouse: response.Video.productionhouseName,
             msisdn: mobile,
-            user_id: userId
+            user_id: userId,
+            Region: response.Video.RegionName,
+            Format: response.Video.FormatName
         });
     }
 }
