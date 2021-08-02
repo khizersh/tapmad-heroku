d0f6a54deeda1dd120433bfe14b615b2004c88d0
Ali Memon <alimemonsd@gmail.com>
2021-08-02 17:46:37 +0500

    Added DNS lookup work

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import HomePage from "../modules/home/components/HomePage";
 import requestIp from "request-ip";
 
 import { HomeService } from "../modules/home/components/home.service";
+import isGoogle from "../services/google-dns-lookup";
 
 export default function Home(props) {
   return (
@@ -32,6 +33,14 @@ export async function getServerSideProps(context) {
   var ip = requestIp.getClientIp(context.req);
   if (process.env.TAPENV == "local") {
     ip = "39.44.217.70";
+  }
+  try {
+    const isGoogleDNS = await isGoogle(ip);
+    if (isGoogleDNS == true) {
+      ip = "39.44.217.70";
+    }
+  } catch (err) {
+    console.log(err);
   }
   console.log("Env is ", process.env.TAPENV)
   let movie, banner, featured;
