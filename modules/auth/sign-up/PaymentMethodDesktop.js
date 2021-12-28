bbb33b69d858e67254577daef86f4d266cb86718
Ali Wahab <ali.memon@tapmad.com>
2021-12-28 13:04:42 +0500

    Signup page responsive fix by Ibrahim

--- modules/auth/sign-up/PaymentMethodDesktop.js
+++ modules/auth/sign-up/PaymentMethodDesktop.js
@@ -55,6 +55,16 @@ export default function PaymentMethodDesktop() {
           }
         `}
       </style>
+      <style>
+        {`
+        .gridCol {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 8px;
+            flex-basis: 0;
+          }
+        `}
+      </style>
       <div className="row padding-signup-layout">
         <div className="col-5">
           <div className="d-flex justify-content-around">
@@ -75,12 +85,13 @@ export default function PaymentMethodDesktop() {
               : null}
           </div>
           <div className="text-grey">{CurrentPackage?.ContentDescription}</div>
-          <div
-            className="mt-3"
-            style={{ color: "#FC5656", fontSize: "0.8em", fontWeight: 300 }}
-          >
-            {CurrentPackage?.HighlightDescription}
-          </div>
+        <div
+          className="mt-3"
+          style={{ color: "#FC5656", fontSize: "0.8em", fontWeight: 300 }}
+          dangerouslySetInnerHTML={{
+            __html: CurrentPackage?.HighlightDescription,
+          }}
+        />
         </div>
         <div className="col-7 border-dotted-left">
           <div className="row">
@@ -114,7 +125,6 @@ export default function PaymentMethodDesktop() {
                           className="radio-cstm"
                           htmlFor={m.PaymentMethodName}
                           style={{ width: "100px" }}
-                          
                         >
                           <div
                             onClick={() => UpdatePaymenthMethod(m)}
--- modules/auth/sign-up/payment-info-components/JazzCashForm.js
+++ modules/auth/sign-up/payment-info-components/JazzCashForm.js
@@ -25,49 +25,37 @@ const JazzCashForm = ({ mobileCode, onChangeNumber, onChangeCnic, logo }) => {
 
   return (
     <>
-      <style jsx>
-        {`
-          .gridCol {
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            gap: 8px;
-            flex-basis: 0;
-          }
-        `}
-      </style>
       <div class="input-group mb-3">
-        <div className="d-flex flex-wrap">
-          <div className="input-group-prepend">
-            <span className="payment-icon border-curve">{mobileCode}</span>
-          </div>
-          <div className="pl-2 flex-grow-1 flex-shrink-1 gridCol">
-            <input
-              type="text"
-              maxLength="10"
-              minLength="10"
-              className="form-control border-curve flex-grow-1 w-100"
-              placeholder="3xxxxxxxxxx"
-              inputMode="numeric"
-              readOnly={SignUpState.LoggedIn ? true : false}
-              value={
-                SignUpState.LoggedIn == 1 ? Cookie.getCookies("user_mob") : num
-              }
-              defaultValue={
-                SignUpState.LoggedIn == 1 ? Cookie.getCookies("user_mob") : num
-              }
-              onChange={(e) => onChange(e)}
-            />
-            <input
-              type="text"
-              maxLength="6"
-              minLength="6"
-              className="form-control border-curve flex-grow-1 w-100"
-              placeholder="Last 6 digits of your CNIC"
-              inputMode="numeric"
-              value={cnic}
-              onChange={(e) => onChangeNic(e)}
-            />
-          </div>
+        <div className="input-group-prepend">
+          <span className="payment-icon border-curve">{mobileCode}</span>
+        </div>
+        <div className="pl-2 flex-grow-1 flex-shrink-1 gridCol">
+          <input
+            type="text"
+            maxLength="10"
+            minLength="10"
+            className="form-control border-curve flex-grow-1 w-100"
+            placeholder="3xxxxxxxxxx"
+            inputMode="numeric"
+            readOnly={SignUpState.LoggedIn ? true : false}
+            value={
+              SignUpState.LoggedIn == 1 ? Cookie.getCookies("user_mob") : num
+            }
+            defaultValue={
+              SignUpState.LoggedIn == 1 ? Cookie.getCookies("user_mob") : num
+            }
+            onChange={(e) => onChange(e)}
+          />
+          <input
+            type="text"
+            maxLength="6"
+            minLength="6"
+            className="form-control border-curve flex-grow-1 w-100"
+            placeholder="Last 6 digits of your CNIC"
+            inputMode="numeric"
+            value={cnic}
+            onChange={(e) => onChangeNic(e)}
+          />
         </div>
       </div>
     </>
