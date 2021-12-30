ef912dd554e7a53f460d24dcd8c346f469976945
khizer <khizer.younus@tapmad.com>
2021-12-30 13:12:11 +0500

    current package screen

--- contexts/auth/AuthContext.js
+++ contexts/auth/AuthContext.js
@@ -31,7 +31,6 @@ export default function AuthProviderNew({ children }) {
     var packages;
     let userId = Cookie.getCookies("userId");
     const pathname = window?.location?.pathname;
-    console.log("pathname : ",pathname);
     if (
       (AuthState.UpdatePackage && userId) ||
       pathname.includes("change-package")
@@ -50,7 +49,7 @@ export default function AuthProviderNew({ children }) {
       dispatch({ type: SET_LOGIN_OPERATORS, data: packages.LoginOperators });
       dispatch({ type: CREDIT_CARD_TYPE, data: packages.CreditCardType });
     }
-  }, [AuthState.UpdatePackage]);
+  }, [AuthState.UpdatePackage , AuthState.callChangePackageApi]);
 
   const data = {
     AuthState,
