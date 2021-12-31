8fedca180d96f0efc75a2a9bcc25f44fbc8a30fc
khizer <khizer.younus@tapmad.com>
2021-12-31 14:15:38 +0500

    ip on play

--- pages/play/[...slug].js
+++ pages/play/[...slug].js
@@ -94,7 +94,9 @@ export async function getServerSideProps(context) {
   //   console.log(err);
   // }
 
-  ip = "39.44.217.70";
+  if (process.env.TAPENV == "local") {
+    ip = "39.44.217.70";
+  }
 
   let url = getRelatedChannelsOrVODs(OriginalMovieId, isChannel);
   const data = await get(url, ip);
