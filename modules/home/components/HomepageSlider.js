461b537899d105cf2a54ce406e8518c97119f6b8
khizer <khizer.younus@tapmad.com>
2022-01-07 12:04:35 +0500

    test live banner

--- modules/home/components/HomepageSlider.js
+++ modules/home/components/HomepageSlider.js
@@ -16,6 +16,7 @@ const Slider = dynamic(() => import("react-slick"));
 const HomePageAd = dynamic(() => import("./HomePageAd"), { ssr: false });
 
 const HomepageSlider = ({ movies, ads, name }) => {
+  console.log("slider : ",movies);
   const router = useRouter();
   var settings = basicSliderConfig(8);
   const [clientXonMouseDown, setClientXonMouseDown] = React.useState(null);
