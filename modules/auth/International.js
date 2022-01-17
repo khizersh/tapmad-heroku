bc026a6f55ab5aaac8e214a37b7dbbd76fa5470b
Ibrahim Aftab <ibrahim.aftab@tapmad.com>
2022-01-17 12:48:19 +0500

    internation form

--- modules/auth/International.js
+++ modules/auth/International.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const International = () => {
   const [screenBg, setScreenBg] = useState("desktop");
+  const { mobileNo, setMobileNo } = useState("");
   useEffect(() => {
     screen.width < 768 && setScreenBg("mobile");
   }, []);
@@ -11,10 +12,19 @@ const International = () => {
         {`
           .intlbg {
             background-position: top center;
-            background-size: 1600px auto;
+            background-size: cover;
             background-repeat: no-repeat;
-            width: 100vw;
-            height: 100vh;
+            min-width: 100vw;
+            min-height: 100vh;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .intlbg form input {
+            line-height: 48px;
+            height: 48px;
           }
         `}
       </style>
@@ -22,8 +32,28 @@ const International = () => {
         style={{
           backgroundImage: `url('/images/signup/signup-${screenBg}-bg.png')`,
         }}
-        className="intlbg"
-      ></section>
+        className="intlbg container-fluid"
+      >
+        <form className="text-center">
+          <h1 className="line-1 mb-0">
+            Wait is over! Watch Pakistan's Biggest <br />{" "}
+            <span className="text-base">Cricket Event HBL PSL 7</span>
+          </h1>
+          <p className="h4 font-weight-normal mb-3">
+            Bring the stadium feel at home.{" "}
+            <span className="text-base">#LevelHai</span>
+          </p>
+          <input
+            type="text"
+            name="mobile"
+            placeholder="Enter your mobile number"
+            value={mobileNo}
+            onChange={(e) => setMobileNo(e.target.value)}
+            width={550}
+            className="mw-100 form-control rounded"
+          />
+        </form>
+      </section>
     </>
   );
 };
