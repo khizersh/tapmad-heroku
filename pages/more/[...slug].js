972a210a3e1d0a405de7de9906bcdce6a120c06a
khizer <khizer.younus@tapmad.com>
2022-01-20 13:55:02 +0500

    log in more

--- pages/more/[...slug].js
+++ pages/more/[...slug].js
@@ -8,6 +8,7 @@ import { IsLiveChannel } from "../../services/constants";
 
 export default function ViewMore(props) {
 
+    console.log("props in more : ",props);
     return <div className="row mx-auto">
         {props.data.Sections == null ?
             <div className="col-12"><h3 className="text-center">No data found</h3></div>
